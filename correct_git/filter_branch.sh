f4f5f52c5ea878979e68d5678e55e2177a49e2ec
kazuki-nisshin <nisshin@dtdsh.com>
2017-11-16 14:39:13 +0900

    Gitの修正ファイルを追加

---
+++ correct_git/filter_branch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 配列で抹消ファイルをセット
+TARGETS=(
+	"sql/2017-11-07.sql"
+	"sql/2017-11-07.zip.tmp"
+	"2017-10-10.sql"
+	"sql/2017-11-01.sql"
+	"assets/img/everest.jpg"
+)
+
+# 半角スペースでjoinする
+target=$(printf " %s" "${TARGETS[@]}")
+target=${target:1}
+
+# 指定したファイルの履歴を抹消する
+git filter-branch --index-filter "git rm -r --cached --ignore-unmatch ${target}" -- --all
